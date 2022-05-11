2cf0bae7ecfa7b1c5fd808473fd355c24ffd048c
christine-tiongson <ctiongson@cbps.canon.com>
2022-05-11 17:04:19 +0800

    Initial commit w/ material theme

---
+++ src/app/modules/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from '../components/main/app.component';
+
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
